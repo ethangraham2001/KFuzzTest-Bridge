a8469827de361807ccf82f3c88b551c8f94c1edd
Ethan Graham <ethangraham@google.com>
2025-08-27 17:31:29 +0000

    work more on parser

--- kfuzztest_input_parser.c
+++ kfuzztest_input_parser.c
@@ -1,6 +1,7 @@
 #include "kfuzztest_input_lexer.h"
 
 #include <string.h>
+#include <stdio.h>
 
 enum ast_node_type {
 	NODE_PROGRAM,
@@ -10,6 +11,11 @@ enum ast_node_type {
 };
 
 struct ast_node; /* Forward declaration. */
+
+struct ast_program {
+	struct ast_node **members;
+	size_t num_members;
+};
 
 struct ast_region {
 	const char *name;
@@ -28,9 +34,10 @@ struct ast_primitive {
 struct ast_node {
 	enum ast_node_type type;
 	union {
+		struct ast_program program;
 		struct ast_region region;
-		struct ast_pointer pointer;
 		struct ast_primitive primitive;
+		struct ast_pointer pointer;
 	} data;
 };
 
@@ -52,6 +59,14 @@ static struct token *advance(struct parser *p)
 	return tok;
 }
 
+static struct token *consume(struct parser *p, enum token_type type, const char *err_msg)
+{
+	if (peek(p)->type != type) {
+		printf("%s\n", err_msg);
+		return NULL;
+	}
+	return advance(p);
+}
 static void retreat(struct parser *p)
 {
 	p->curr_token--;
@@ -87,6 +102,8 @@ static struct ast_node *parse_primitive(struct parser *p)
 	}
 
 	ret = malloc(sizeof(*ret));
+	if (!ret)
+		return NULL;
 	ret->type = NODE_PRIMITIVE;
 	ret->data.primitive.byte_width = byte_width;
 	return ret;
@@ -96,16 +113,16 @@ static struct ast_node *parse_ptr(struct parser *p)
 {
 	struct ast_node *ret;
 	struct token *tok;
-	if (advance(p)->type != TOKEN_KEYWORD_PTR)
+	if (!consume(p, TOKEN_KEYWORD_PTR, "expected 'ptr'"))
 		return NULL;
-	if (advance(p)->type != TOKEN_LBRACKET)
+	if (!consume(p, TOKEN_LBRACKET, "expected '['"))
 		return NULL;
 
 	tok = advance(p);
 	if (tok->type != TOKEN_IDENTIFIER)
 		return NULL;
 
-	if (advance(p)->type != TOKEN_RBRACKET)
+	if (!consume(p, TOKEN_RBRACKET, "expected ']'"))
 		return NULL;
 
 	ret = malloc(sizeof(*ret));
@@ -122,14 +139,17 @@ static struct ast_node *parse_type(struct parser *p)
 	if (peek(p)->type == TOKEN_KEYWORD_PTR) {
 		return parse_ptr(p);
 	}
+	return NULL;
 }
 
 static struct ast_node *parse_region(struct parser *p)
 {
+	struct token *tok, *identifier;
+	struct ast_region *region;
 	enum token_type tok_type;
 	struct ast_node *node;
 	struct ast_node *ret;
-	struct token *tok;
+	int i;
 
 	if (!match(p, TOKEN_IDENTIFIER))
 		return NULL;
@@ -138,14 +158,77 @@ static struct ast_node *parse_region(struct parser *p)
 	if (!ret)
 		return NULL;
 
-	advance(p); // Identifier;
+	identifier = advance(p);
 	tok = advance(p);
 	if (tok->type != TOKEN_LBRACE)
-		return NULL;
+		goto fail_early;
 
-	ret->data.region.name = strndup(tok->data.identifier.start, tok->data.identifier.length);
+	region = &ret->data.region;
+	region->name = strndup(identifier->data.identifier.start, identifier->data.identifier.length);
+	if (!region->name)
+		goto fail_early;
+
+	region->num_members = 0;
 	while (!match(p, TOKEN_RBRACE)) {
 		node = parse_type(p);
-		/* TODO: add to members. */
+		if (!node)
+			goto fail;
+		/* TODO: Handle realloc failure. */
+		region->members = realloc(region->members, ++region->num_members * sizeof(struct ast_node *));
+		region->members[region->num_members - 1] = node;
 	}
+
+	if (!consume(p, TOKEN_RBRACE, "expected '}'"))
+		goto fail;
+
+	ret->type = NODE_REGION;
+	return ret;
+
+fail:
+	for (i = 0; i < region->num_members; i++)
+		free(region->members[i]);
+	free((void *)region->name);
+	free(region->members);
+fail_early:
+	free(ret);
+	return NULL;
+}
+
+static struct ast_node *parse_program(struct parser *p)
+{
+	struct ast_program *prog;
+	struct ast_node *reg;
+	struct ast_node *ret;
+	int i;
+
+	ret = malloc(sizeof(*ret));
+	if (!ret)
+		return NULL;
+	ret->type = NODE_PROGRAM;
+
+	prog = &reg->data.program;
+	prog->num_members = 0;
+	prog->members = NULL;
+	while (!match(p, TOKEN_EOF)) {
+		reg = parse_region(p);
+		if (!reg)
+			goto fail;
+		/* TODO: Handle realloc failure. */
+		prog->members = realloc(prog->members, ++prog->num_members * sizeof(struct ast_node *));
+	}
+
+	return ret;
+
+fail:
+	for (i = 0; i < prog->num_members; i++)
+		free(prog->members[i]);
+	free(prog->members);
+	free(ret);
+	return NULL;
+}
+
+static struct ast_node *parse(struct token **tokens, size_t token_count)
+{
+	struct parser p = { .tokens = tokens, .token_count = token_count, .curr_token = 0 };
+	return parse_program(&p);
 }
