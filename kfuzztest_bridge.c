a4b6e6f0932fab47e6aaa2a21bf90801db18d5f3
Ethan Graham <ethangraham@google.com>
2025-08-28 15:13:04 +0000

    refactor main file

--- kfuzztest_bridge.c
+++ kfuzztest_bridge.c
@@ -5,25 +5,19 @@
  * Copyright 2025 Google LLC
  */
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "kfuzztest_input_lexer.h"
 #include "kfuzztest_input_parser.h"
 #include "kfuzztest_encoder.h"
 #include "debug.h"
 
-const char *input = "bar { u32 ptr[foo] }; foo { ptr[data] }; data { arr[u8, 42] };";
-
-enum token_type expected[] = {
-	TOKEN_IDENTIFIER, TOKEN_LBRACE,	     TOKEN_KEYWORD_ARR, TOKEN_LBRACKET,	   TOKEN_KEYWORD_U8,
-	TOKEN_COMMA,	  TOKEN_INTEGER,     TOKEN_RBRACKET,	TOKEN_RBRACE,	   TOKEN_IDENTIFIER,
-	TOKEN_LBRACE,	  TOKEN_KEYWORD_PTR, TOKEN_LBRACKET,	TOKEN_IDENTIFIER,  TOKEN_RBRACKET,
-	TOKEN_RBRACE,	  TOKEN_IDENTIFIER,  TOKEN_LBRACE,	TOKEN_KEYWORD_U32, TOKEN_KEYWORD_PTR,
-	TOKEN_LBRACKET,	  TOKEN_IDENTIFIER,  TOKEN_RBRACKET,	TOKEN_RBRACE,	   TOKEN_EOF,
-};
-
 const char *usage_str = "usage: "
 			"./kfuzztest-bridge <program-description> <fuzz-target-name> <input-file>\n"
 			"for more detailed information see <docs>";
+
+static int invoke_one(const char *input_fmt, const char *fuzz_target, const char *input_filepath);
 
 static void usage()
 {
@@ -32,8 +26,6 @@ static void usage()
 
 int main(int argc, char *argv[])
 {
-	struct token **tokens;
-	size_t num_tokens;
 	int ret;
 	int i;
 
@@ -42,16 +34,54 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	ret = tokenize(argv[1], &tokens, &num_tokens);
+	return invoke_one(argv[1], argv[2], argv[3]);
+}
+
+static int invoke_kfuzztest_target(const char *target_name, const char *data, size_t data_size)
+{
+	ssize_t bytes_written;
+	char buf[256];
+	int ret;
+	int fd;
+
+	ret = snprintf(buf, sizeof(buf), "/sys/kernel/debug/kfuzztest/%s/input", target_name);
+	if (ret < 0)
+		return ret;
+
+	fd = openat(AT_FDCWD, buf, O_WRONLY, 0);
+	if (fd < 0)
+		return fd;
+
+	bytes_written = write(fd, (void *)data, data_size);
+	if (bytes_written < 0) {
+		close(fd);
+		return bytes_written;
+	}
+
+	if (close(fd) != 0)
+		return 1;
+	return 0;
+}
+
+static int invoke_one(const char *input_fmt, const char *fuzz_target, const char *input_filepath)
+{
+	struct ast_node *ast_prog;
+	struct rand_stream *rs;
+	struct token **tokens;
+	size_t num_tokens;
+	size_t num_bytes;
+	char *bytes;
+	int ret;
+
+	ret = tokenize(input_fmt, &tokens, &num_tokens);
 	if (ret)
 		return ret;
 
-	struct ast_node *result = parse(tokens, num_tokens);
-	visualize_ast(result);
+	ast_prog = parse(tokens, num_tokens);
 
-	struct rand_stream *r = new_rand_stream(argv[3], 1024);
-	size_t num_bytes;
-	char *bytes = encode(result, r, &num_bytes);
+	rs = new_rand_stream(input_filepath, 1024);
+	bytes = encode(ast_prog, rs, &num_bytes);
 	print_bytes(bytes, num_bytes);
-	return 0;
+
+	return invoke_kfuzztest_target(fuzz_target, bytes, num_bytes);
 }
